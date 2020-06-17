445160d28ac0a1a07af1100537a3238a66798263
Kevinrwh <50501047+Kevinrwh@users.noreply.github.com>
2020-06-16 22:37:45 -0400

    Add files via upload

---
+++ beerCalc.java
@@ -0,0 +1,57 @@
+/* Kevin Ramos
+ * COP3330
+ * HW2: Beer Assignment
+ * This program calculates the cost of 
+ * and potential weight gain due to beer in a year
+*/
+
+package BeerCalc;
+
+import java.util.Scanner;
+
+public class beerCalc {
+
+	public static void main(String[] args) {
+//		On average, how many beers will you consume each day?
+//				2.5
+//
+//		On average, how much will you pay for each can of beer?
+//				1.25
+//		That is approximately 912.50 beers in one year.
+//		In one year, you will consume approximately 136875.00 calories from beer alone.
+		
+//		Without diet or exercise to counter these calories, you can expect to gain 37.50 pounds 
+//		from drinking that much beer this year.
+		
+		Scanner beerInfo = new Scanner(System.in);
+		
+		double dailyBeers, calTotal, beerTotal;
+		double weightGain, beerExpense, beerCost;
+		double beerCal = 150;
+		
+		System.out.println("On average, how many beers will you consume each day?");
+		dailyBeers = beerInfo.nextDouble();
+		
+		System.out.println("On average, how much will you pay for each can of beer?");
+		beerCost = beerInfo.nextDouble();
+		
+		// Calculate beers consumed in a year.
+		beerTotal = dailyBeers * 365;
+		
+		// Output total beers consumed over the year.
+		System.out.printf("This is approximately %.2f beers over the course of the year.\n", +beerTotal);
+		
+		// Calculate calorie intake due to beer alone and corresponding weight gain
+		calTotal = beerTotal * beerCal;
+		weightGain = calTotal/3500.00;
+		
+		// Output calories consumed and corresponding weight gain
+		System.out.printf("In one year you'll consume %.2f calories due to beer alone.\n", +calTotal);
+		System.out.printf("Without a change in diet or exercise, you will gain %.2f pounds from drinking that much beer this year.\n", +weightGain);
+		
+		// Calculate cost of beer over a year
+		beerExpense = beerTotal * beerCost; 
+		System.out.printf("You will pay $%.2f for beer this year.", +beerExpense);	
+	}
+
+}
